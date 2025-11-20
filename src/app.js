6df83af5082dccdd6334faf8ddac2b3285078a88
Nama Kamu <emailkamu@example.com>
2025-11-20 21:18:00 +0700

    add: auth/login

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const movieRoutes = require("./routes/movieRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -7,6 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
 app.use("/api/auth", authRoutes);
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // REGISTER USER
 exports.register = async (req, res) => {
@@ -37,5 +38,45 @@ exports.register = async (req, res) => {
       message: "Gagal mendaftarkan user",
       error,
     });
+  }
+};
+
+// LOGIN USER
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email dan password harus diisi" });
+    }
+
+    // Query database
+    const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "Email atau password salah" });
+    }
+
+    const user = rows[0];
+
+    // Bandingkan password
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).json({ message: "Email atau password salah" });
+    }
+
+    // Generate token
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    delete user.password;
+
+    return res.status(200).json({
+      message: "Login berhasil",
+      user,
+      token,
+    });
+  } catch (err) {
+    console.error("LOGIN ERROR:", err);
+    return res.status(500).json({ message: "Server error", error: err });
   }
 };
